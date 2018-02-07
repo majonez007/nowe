5396c3c8d9cff0b3b32a531085b1d9c64d4af811
majonez007 <maj0nez@poczta.fm>
2018-02-07 10:54:32 +0100

    fibonnacci

---
+++ fibonnaci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fibonacci(int index)
+{
+	if(index>1)
+		return fibonacci(index-1)+fibonacci(index-2);
+	else
+		return index;
+}
+
+int main(){
+	int index;
+
+	printf("Podaj indeks w ciagu Fibonacciego: ");
+	scanf("%d", &index);
+
+	if(index>=0) printf("F(%d) = %d\n", index, fibonacci(index));
+
+	return 0;
+}
